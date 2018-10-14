fed242ce22dcef6f5677c39e50c2d3f226cb63ef
Kirill <afanasiev.kirill@gmail.com>
2018-10-14 18:47:00 +0300

    Добавлено логирование

--- myfantasyleague/src/test/java/ru/home/pft/myfantasyleague/tests/TestBase.java
+++ myfantasyleague/src/test/java/ru/home/pft/myfantasyleague/tests/TestBase.java
@@ -1,13 +1,22 @@
 package ru.home.pft.myfantasyleague.tests;
 
+import com.sun.deploy.uitoolkit.impl.text.TextConsoleWindow;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.home.pft.myfantasyleague.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 
 public class TestBase {
 
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
   protected final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
@@ -18,9 +27,19 @@ public class TestBase {
     app.init();
   }
 
+  @BeforeMethod()
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + "with parameters " + Arrays.asList(p));
+  }
+
   @AfterSuite(alwaysRun = true)
   public void tearDown() throws Exception {
     app.stop();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m, Object[] p) {
+    logger.info("Stop test " + m.getName() + "with parameters " + Arrays.asList(p));
   }
 
   public ApplicationManager getApp() {
