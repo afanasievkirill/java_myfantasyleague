2e0f86314991e4fdf045be2dc2da9123c3a28284
Kirill <afanasiev.kirill@gmail.com>
2018-10-14 19:56:53 +0300

    Test Data provider

--- myfantasyleague/src/test/java/ru/home/pft/myfantasyleague/tests/CustomizeInformation.java
+++ myfantasyleague/src/test/java/ru/home/pft/myfantasyleague/tests/CustomizeInformation.java
@@ -2,6 +2,7 @@ package ru.home.pft.myfantasyleague.tests;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.home.pft.myfantasyleague.model.CustomizeData;
 
@@ -9,12 +10,25 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CustomizeInformation extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions(){
     app.goTo().customizeTD();
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validCustomizeInformation(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new CustomizeData()
+            .withAbbriveation("SMB").withStadium("OneMileHigh").withNote("Позитивный тест")
+            .withFranchiseIcon(new File("src/test/resources/logo.png")).withTimezone("CT")});
+    return list.iterator();
   }
 
   @Test
